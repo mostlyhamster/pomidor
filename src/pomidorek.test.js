a0e2214146f0c366ed78b82249ece046ccade738
Riv <rivian@protonmail.com>
2020-03-14 20:15:40 +0100

    Test pause default

--- src/pomidorek.test.js
+++ src/pomidorek.test.js
@@ -3,3 +3,7 @@ import Pomidorek from './pomidorek.js'
 test('Test if tests are working', () => {
   expect(true).toEqual(true)
 })
+
+test('Pomidorek starts out paused by default', () => {
+  expect(Pomidorek.isPaused()).toEqual(true)
+})
