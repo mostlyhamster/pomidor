be9bc25b92060c4b8acf4b69e4ec430d5826c685
Riv <rivian@protonmail.com>
2020-03-14 19:05:41 +0100

    Cleanup

---
+++ src/pomidorek.js
@@ -0,0 +1,108 @@
+export const activities = {
+    POMODORO: 'pomodoro',
+    SHORT_BREAK: 'short break',
+    LONG_BREAK: 'long break',
+}
+
+
+const DEFAULT_DURATION_POMODORO_SECONDS = 15
+const DEFAULT_DURATION_SHORT_BREAK_SECONDS = 5
+const DEFAULT_DURATION_LONG_BREAK_SECONDS = 10
+
+let state = {
+    timeLeftMillis : 0,
+    lastPulse : new Date(),
+    pause : true,
+    currectActivity : activities.POMODORO,
+    onPulse: (timeLeftMillis) => true
+}
+
+
+let pulse = null
+const init = () => {
+    clearInterval(pulse)
+    pulse = setInterval(() => {
+        doCountdown();
+     }, 500)
+     reset()
+}
+
+const addTime = (timeMillis) => {
+    state.timeLeftMillis += timeMillis
+}
+
+const getActivityDurationInSeconds = (activity) => {
+    console.log('check activity ', activity)
+    switch (activity) {
+        case activities.POMODORO:
+            return DEFAULT_DURATION_POMODORO_SECONDS
+        case activities.SHORT_BREAK:
+            return DEFAULT_DURATION_SHORT_BREAK_SECONDS
+        case activities.LONG_BREAK:
+            return DEFAULT_DURATION_LONG_BREAK_SECONDS
+        default:
+            throw new Error('Unknown activity: ' + activity);
+    }
+}
+
+/**
+ * @param {Object} activity 
+ */
+const setActivity = (activity, force) => {
+    if (activity === state.currectActivity && force === false) return
+    state.currectActivity = activity;
+    state.timeLeftMillis = getActivityDurationInSeconds(activity) * 1000
+    state.lastPulse = new Date()
+    state.lastPulse.setMilliseconds(999)
+    doCountdown()
+}
+
+const doCountdown = () => {
+    const now = new Date()
+    if (!state.pause) {
+        state.timeLeftMillis = state.timeLeftMillis - (now - state.lastPulse)
+    }
+    state.lastPulse = now;
+    state.onPulse(state.timeLeftMillis)
+}
+
+const buildTimerValue = () => {
+    const time = new Date(state.timeLeftMillis)
+    if (state.timeLeftMillis <= 0) {
+        return '00:00'
+    } else {
+        return new String(time.getMinutes()).padStart(2, '0') + ':'  + new String(time.getSeconds()).padStart(2, '0')
+    }
+}
+
+const reset = () => {
+    setActivity(state.currectActivity, true)
+    pause()
+}
+
+const pause = () => {
+    console.log('pause')
+    if(state.pause) return;
+    state.pause = true
+}
+
+
+const unpause = () => {
+    console.log('unpause')
+    state.pause = false
+}
+
+
+const Pomidorek = {
+    init: init,
+    pause: pause,
+    unpause: unpause,
+    isPaused: () => state.pause,
+    reset: reset,
+    setActivity: setActivity,
+    addTime: addTime,
+    getTimeRemaining: () => state.timeLeftMillis,
+    setPulseCallback: (callback) => state.onPulse = callback,
+}
+
+export default Pomidorek
--- src/render.js
+++ src/render.js
@@ -1,24 +1,6 @@
-/**
- * Constants
- */
-const DEFAULT_DURATION_POMODORO_SECONDS = 15
-const DEFAULT_DURATION_SHORT_BREAK_SECONDS = 5
-const DEFAULT_DURATION_LONG_BREAK_SECONDS = 10
+import  Pomidorek, { activities } from './pomidorek.js'
 
-const activities = {
-    POMODORO: 'pomodoro',
-    SHORT_BREAK: 'short break',
-    LONG_BREAK: 'long break',
-}
-
-/**
- * Main timer element
- */
 const timerElement = document.getElementById('timer')
-
-/**
- * Activity buttons
- */
 const buttonShortBreak = document.getElementById('button-activity-break-short')
 const buttonLongBreak = document.getElementById('button-activity-break-long')
 const buttonPomodoro = document.getElementById('button-activity-pomodoro')
@@ -26,94 +8,43 @@ const buttonPomodoro = document.getElementById('button-activity-pomodoro')
 const activityButtons = [buttonShortBreak, buttonLongBreak, buttonPomodoro]
 
 buttonShortBreak.addEventListener('click', (event) => {
-    setActivity(activities.SHORT_BREAK, false)
+    Pomidorek.setActivity(activities.SHORT_BREAK, false)
     activityButtons.forEach(btn => btn.classList.remove('is-active'))
     buttonShortBreak.classList.add('is-active')
 });
 
 buttonLongBreak.addEventListener('click', (event) => {
-    setActivity(activities.LONG_BREAK, false)
+    Pomidorek.setActivity(activities.LONG_BREAK, false)
     activityButtons.forEach(btn => btn.classList.remove('is-active'))
     buttonLongBreak.classList.add('is-active')
 });
 
 buttonPomodoro.addEventListener('click', (event) => {
-    setActivity(activities.POMODORO, false)
+    Pomidorek.setActivity(activities.POMODORO, false)
     activityButtons.forEach(btn => btn.classList.remove('is-active'))
     buttonPomodoro.classList.add('is-active')
 });
 
-/**
- * @param {Object} activity 
- */
-const setActivity = (activity, force) => {
-    if (activity === currectActivity && force === false) return
-    currectActivity = activity;
-    timeLeftMillis = getActivityDurationInSeconds(activity) * 1000
-    lastPulse = new Date()
-    lastPulse.setMilliseconds(999)
-    doCountdown()
-}
 
-const getActivityDurationInSeconds = (activity) => {
-    console.log('check activity ', activity)
-    switch (activity) {
-        case activities.POMODORO:
-            return DEFAULT_DURATION_POMODORO_SECONDS
-        case activities.SHORT_BREAK:
-            return DEFAULT_DURATION_SHORT_BREAK_SECONDS
-        case activities.LONG_BREAK:
-            return DEFAULT_DURATION_LONG_BREAK_SECONDS
-        default:
-            throw new Error('Unknown activity: ' + activity);
-    }
-}
-/*
- * Function buttons
- */
+
 const buttonResetActivity = document.getElementById('button-activity-reset')
 const buttonStartActivity = document.getElementById('button-activity-start')
 const buttonNextActivity = document.getElementById('button-activity-next')
 const buttonIncreaseActivityDuration = document.getElementById('button-activity-increase-duration')
 
-/**
- * State
- */
-let timeLeftMillis = 0;
-let lastPulse = new Date()
-let isPaused = true;
-let currectActivity = activities.POMODORO
-
-/**
- * Button behaviour
- */
 buttonStartActivity.addEventListener('click', (event) => {
-    isPaused ? unpause() : pause()
+    Pomidorek.isPaused() ? Pomidorek.unpause() : Pomidorek.pause()
 })
 
 buttonIncreaseActivityDuration.addEventListener('click', (event) => {
-    timeLeftMillis += 1 * 60 * 1000
-    doCountdown()
+    Pomidorek.addTime(1 * 60 * 1000)
 })
 
 buttonResetActivity.addEventListener('click', (event) => {
-    reset()
+    Pomidorek.reset()
 })
 
-setInterval(() => {
-   doCountdown();
-}, 500)
-
-const doCountdown = () => {
-    const now = new Date()
-    if (!isPaused) {
-        timeLeftMillis = timeLeftMillis - (now - lastPulse)
-    }
-    lastPulse = now;
-    updateTimerValue()
-}
-
-const updateTimerValue = () => {
+const updateTimerValue = (timeLeftMillis) => {
     const time = new Date(timeLeftMillis)
     if (timeLeftMillis <= 0) {
         timerElement.innerText = '00:00'
@@ -124,24 +55,6 @@ const updateTimerValue = () => {
             + new String(time.getSeconds()).padStart(2, '0')
     }
 }
+Pomidorek.init();
+Pomidorek.setPulseCallback(updateTimerValue)
 
-const reset = () => {
-    setActivity(currectActivity, true)
-    pause()
-}
-
-const pause = () => {
-    console.log('pause')
-    if(isPaused) return;
-    isPaused = true
-    buttonStartActivity.innerText = 'Start';
-}
-
-
-const unpause = () => {
-    console.log('unpause')
-    isPaused = false
-    buttonStartActivity.innerText = 'Pause'
-}
-
-reset()
