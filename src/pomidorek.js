bb5857136db2df0fc0983af4c6c5e74653a3737e
Riv <rivian@protonmail.com>
2020-03-26 12:46:23 +0100

    Adjust pulse interval to make the timer smoother

--- src/pomidorek.js
+++ src/pomidorek.js
@@ -4,16 +4,17 @@ export const activities = {
     LONG_BREAK: 'long break',
 }
 
+const PULSE_INTERVAL_MILLIS = 499
 
 const DEFAULT_DURATION_POMODORO_SECONDS = 25 * 60
 const DEFAULT_DURATION_SHORT_BREAK_SECONDS = 5 * 60
 const DEFAULT_DURATION_LONG_BREAK_SECONDS = 15 * 60
 
 let state = {
-    timeLeftMillis : 0,
-    lastPulse : new Date(),
-    pause : true,
-    currectActivity : activities.POMODORO,
+    timeLeftMillis: 0,
+    lastPulse: new Date(),
+    pause: true,
+    currectActivity: activities.POMODORO,
     activityComplete: false,
     onPulse: (timeLeftMillis) => true,
     onActivityChanged: (newActivity) => true,
@@ -28,8 +29,8 @@ const start = () => {
     clearInterval(pulseInterval)
     pulseInterval = setInterval(() => {
         pulse();
-     }, 500)
-     reset()
+    }, PULSE_INTERVAL_MILLIS)
+    reset()
 }
 
 const addTime = (timeMillis) => {
@@ -50,9 +51,6 @@ const getActivityDurationInSeconds = (activity) => {
     }
 }
 
-/**
- * @param {Object} activity 
- */
 const setActivity = (activity, force) => {
     if (activity === state.currectActivity && force === false) return
     state.currectActivity = activity;
@@ -84,7 +82,7 @@ const getSuggestedTimerValue = () => {
     if (state.timeLeftMillis <= 0) {
         return '00:00'
     } else {
-        return new String(time.getMinutes()).padStart(2, '0') + ':'  + new String(time.getSeconds()).padStart(2, '0')
+        return new String(time.getMinutes()).padStart(2, '0') + ':' + new String(time.getSeconds()).padStart(2, '0')
     }
 }
 
@@ -95,7 +93,7 @@ const reset = () => {
 
 const pause = () => {
     console.log('pause')
-    if(state.pause) return;
+    if (state.pause) return;
     state.pause = true
 }
 
@@ -106,7 +104,7 @@ const unpause = () => {
 }
 
 const getNextActivity = () => {
-    switch(state.currectActivity) {
+    switch (state.currectActivity) {
         case activities.POMODORO:
             const lastLongBreakIdx = state.activityLog.lastIndexOf(activities.LONG_BREAK)
             let pomodoroSinceLastLongBreak = 0
@@ -115,7 +113,7 @@ const getNextActivity = () => {
                     pomodoroSinceLastLongBreak++
                 }
             }
-            if (pomodoroSinceLastLongBreak > 3)  return  activities.LONG_BREAK
+            if (pomodoroSinceLastLongBreak > 3) return activities.LONG_BREAK
             return activities.SHORT_BREAK
         case activities.SHORT_BREAK:
         case activities.LONG_BREAK:
@@ -140,9 +138,9 @@ const isBreak = () => {
 }
 
 const showNotification = () => {
-    const msg = isBreak() 
-        ? 'Break is over, time to wørk again' 
-        : 'Awesome, you finished your pomodoro. Have a break!'
+    const msg = isBreak() ?
+        'Break is over, time to wørk again' :
+        'Awesome, you finished your pomodoro. Have a break!'
 
     new Notification("Hello, it's your favorite tomato 🍅", {
         body: msg
@@ -158,7 +156,7 @@ const Pomidorek = {
     addTime: addTime,
     getTimeRemaining: () => state.timeLeftMillis,
     setPulseCallback: (callback) => state.onPulse = callback,
-    setActivityChangedCallback : (callback)  => state.onActivityChanged = callback,
+    setActivityChangedCallback: (callback) => state.onActivityChanged = callback,
     getSuggestedTimerValue: getSuggestedTimerValue,
     getCurrentActivity: () => state.currectActivity,
     getNextActivity: getNextActivity,
